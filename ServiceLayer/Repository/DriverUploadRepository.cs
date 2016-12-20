c5b62ccde7185340b7400db98049dbaeabbdbeaa
SarkisPetrosyan <sarkis.petrosyan1993@gmail.com>
2016-12-20 20:28:37 +0400

    a lot of changes 1. front-end integration 2. some new repositories were added 3. responsive layout using bootstrap and a lot of fuuuuuucking shit

---
+++ ServiceLayer/Repository/DriverUploadRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Context;
+using DAL.Entities;
+
+namespace ServiceLayer.Repository
+{
+    public class DriverUploadRepository : EntityRepository, IDriverUploadRepository
+    {
+        public DriverUploadRepository(IDbContext dbContext) : base(dbContext)
+        {
+
+        }
+
+        public async Task<DriverUpload> GetDriverUploadByDriverIdAsync(int id)
+        {
+            return await DbContext.DriverUploads.FirstOrDefaultAsync(c => c.DriverId == id);
+        }
+    }
+}
