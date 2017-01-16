551afd196460387398da2ad0e5d09e5dcc241bd7
Edward Chopuryan <edward.chopuryan@gmail.com>
2017-01-16 21:55:16 +0400

    added update and get driver location apis

---
+++ ServiceLayer/Repository/DriverLocationRepository.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Threading.Tasks;
+using DAL.Context;
+using DAL.Entities;
+
+namespace ServiceLayer.Repository
+{
+    public class DriverLocationRepository : EntityRepository, IDriverLocationRepository
+    {
+        public DriverLocationRepository(IDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<DriverLocation> GetDriverLocationByDriverIdAsync(int driverId)
+        {
+            return await DbContext.DriverLocations.FirstOrDefaultAsync(c => c.Id == driverId);
+        }
+
+        public async Task UpdateDriverLocation(DriverLocation location)
+        {
+            DbContext.DriverLocations.AddOrUpdate(location);
+            await DbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ ServiceLayer/Service/DriverLocationService.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using DAL.Entities;
+using ServiceLayer.Repository;
+
+namespace ServiceLayer.Service
+{
+    public class DriverLocationService : EntityService, IDriverLocationService
+    {
+        private readonly IDriverLocationRepository _driverLocationRepository;
+        public DriverLocationService(IEntityRepository entityRepository,
+            IDriverLocationRepository repository) : base(entityRepository)
+        {
+            _driverLocationRepository = repository;
+        }
+
+        public async Task<DriverLocation> GetDriverLocationByDriverIdAsync(int driverId)
+        {
+            return await _driverLocationRepository.GetDriverLocationByDriverIdAsync(driverId);
+        }
+
+        public async Task UpdateDriverLocation(DriverLocation location)
+        {
+            await _driverLocationRepository.UpdateDriverLocation(location);
+        }
+    }
+}
