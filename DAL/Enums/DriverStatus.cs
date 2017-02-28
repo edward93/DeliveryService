4efd8cd0233f364f5e92f17f70ea0c931bbdd8c1
Edward <edward.chopuryan@gmail.com>
2017-02-28 18:26:37 +0400

    statuses added

--- DAL/Enums/DriverStatus.cs
+++ DAL/Enums/DriverStatus.cs
@@ -1,10 +1,16 @@
-﻿namespace DAL.Enums
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DAL.Enums
 {
     public enum DriverStatus
     {
         Online = 0,
         Offline,
         Busy,
+        [Display(Name = "No GPS")]
+        NoGps,
+        [Display(Name = "Disconnected From Hub")]
+        DisconnectedFromHub,
         Other
     }
 }
